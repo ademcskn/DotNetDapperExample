41741468f838c663a7b9fb459fbf47241e92a165
ademcskn <ademcskn@hotmail.com>
2021-06-02 14:23:59 +0300

    Add project files.

---
+++ DotNetDapperExample/DapperRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DotNetDapperExample
+{
+    public class DapperRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperRepository()
+        {
+            _connection = new SqlConnection("Data Source=.;Initial Catalog=Customer;Integrated Security=True;");
+        }
+
+        public IDbConnection Connection
+        {
+            get { return _connection; }
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+                _connection.Dispose();
+        }
+    }
+}
---
+++ DotNetDapperExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DotNetDapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (IDbConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Mshowto;Integrated Security=True;"))
+            //{
+            //    conn.Open();
+
+            //    conn.Execute(
+            //        "insert into Customer(Name,Surname) values (@name,@surname)",new Customer { Name="Adem",Surname=",Coşkun"}
+            //        );
+
+            //    List<Customer> customersList = conn.Query<Customer>("select * from Customer").ToList();
+            //    conn.Close();
+            //}
+
+            DapperRepository microOrm = new DapperRepository();
+            var result = microOrm.Connection.Query<Customer>("select * from customer").ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name + " " + item.Surname);
+            }
+            Console.ReadKey();
+        }
+    }
+}
